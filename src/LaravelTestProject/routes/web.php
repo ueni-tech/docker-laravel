b577775d7ece12f0042dd6c265a7f24dcc2c3166
uenishi <ueni.tech@gmail.com>
2024-04-09 10:48:22 +0900

    #5 breeze初期設定

--- src/LaravelTestProject/routes/web.php
+++ src/LaravelTestProject/routes/web.php
@@ -3,6 +3,14 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\HelloController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\VendorController;
+use App\Http\Controllers\RequestController;
+use App\Http\Controllers\ResponseController;
+use App\Http\Controllers\CookieController;
+use App\Http\Controllers\SessionController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -29,3 +37,41 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::get('/hello', [HelloController::class, 'index']);
+
+Route::get('/products', [ProductController::class, 'index']);
+
+Route::get('/products/create', [ProductController::class, 'create']);
+
+Route::post('/products/store', [ProductController::class, 'store'])->name('products.store');
+
+Route::get('/products/{id}', [ProductController::class, 'show']);
+
+Route::get('/vendors/create', [VendorController::class, 'create']);
+
+Route::post('/vendors/store', [VendorController::class, 'store'])->name('vendors.store');
+
+Route::get('/vendors/{id}', [VendorController::class, 'show']);
+
+Route::get('/requests/create', [RequestController::class, 'create']);
+
+Route::post('/requests/confirm', [RequestController::class, 'confirm'])->name('requests.confirm');
+
+Route::get('/responses', [ResponseController::class, 'index']);
+
+Route::get('/cookies', [CookieController::class, 'index']);
+
+Route::get('/cookies/create', [CookieController::class, 'create'])->name('cookies.create');
+
+Route::post('/cookies/store', [CookieController::class, 'store'])->name('cookies.store');
+
+Route::delete('/cookies/destroy', [CookieController::class, 'destroy'])->name('cookies.destroy');
+
+Route::get('/sessions', [SessionController::class, 'index']);
+
+Route::get('/sessions/create', [SessionController::class, 'create'])->name('sessions.create');
+
+Route::post('/sessions/store', [SessionController::class, 'store'])->name('sessions.store');
+
+Route::delete('/sessions/destroy', [SessionController::class, 'destroy'])->name('sessions.destroy');
